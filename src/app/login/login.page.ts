c319d276093b56288e6b75b82f074060709dca8c
o0leslyth40o <102860687+o0leslyth40o@users.noreply.github.com>
2024-09-09 14:53:20 -0300

    creacion formulario, con validaciones... existe un error

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
+
+type loginForm ={
+  correo: FormControl<string>,
+  password: FormControl<string>
+};
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm!: FormGroup<loginForm>;
+  
 
-  constructor() { }
+  constructor(private readonly formBuilder: NonNullableFormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void{
+
+    this.loginForm = new FormGroup<loginForm>({
+      correo: new FormControl('', {nonNullable: true}),
+      password: new FormControl('',{nonNullable:true})
+    });
+
+    this.loginForm = this.formBuilder.group({
+      correo:'',
+      password:''
+    })
   }
 
+  onSubmit() {
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }
+  }
+  
 }
+
